73e316908fdb3a120cbd01c9d5bdad7e47ff0927
desewenkdk <171018since400@gmail.com>
2018-04-14 20:33:59 +0900

    ID

--- MASM1/141506.asm
+++ MASM1/141506.asm
@@ -1,6 +1,5 @@
 TITLE Assingment 2 code : 20141506 ±èµ¿±Ô
 
-
 INCLUDE Irvine32.inc
 
 .data
@@ -16,18 +15,18 @@ main PROC
 	add eax, eax ;12 * val1
 
 	mov ebx, val2
-	add eax, ebx ;;12*val + val2
+	add eax, ebx ;12*val + val2
 	add ebx, ebx
-	sub ecx, ebx ;;store 2val1 - 2val2
-	add eax, ebx ;;12*val + 3*val2
+	sub ecx, ebx ;store 2val1 - 2val2
+	add eax, ebx ;12*val + 3*val2
 	add ebx, ebx
-	sub ecx, ebx ;; store 2val1 - 6val2
+	sub ecx, ebx ; store 2val1 - 6val2
 	add ebx, ebx
 	add ebx, ebx 
-	add eax, ebx ;;12*val + (3+16=19)*val2
+	add eax, ebx ;12*val + (3+16=19)*val2
 	add ebx, ebx
-	sub ecx, ebx ;;store 2val1 - (6+32=38)val2
-	add eax, ebx ;;12val1 + (19+32=51)val2
+	sub ecx, ebx ;store 2val1 - (6+32=38)val2
+	add eax, ebx ;12val1 + (19+32=51)val2
 
 	mov ebx, val4
 	sub ebx, val3 ; (val4 - val3)
@@ -35,12 +34,12 @@ main PROC
 	add ebx, ebx
 	add ebx, ebx
 	add ecx, ebx ; store 2val1 - 38val2 + 4(val4 - val3)
-	sub eax, ebx ; 12val1 + 51val2 - (1+4=5)(val4-val3)
+	sub eax, ebx ; store eval1 = 12val1 + 51val2 - (1+4=5)(val4-val3) to eax
 	add ebx, ebx
 	add ebx, ebx
 	add ecx, ebx ; store 2val1 - 38val2 + (4+16=20)(val4 - val3)
 	mov ebx, eax
-	add ebx, ecx ; calculate (12+2)val1 + (51-38)val2 + (-5+20)(val4-val3)
+	add ebx, ecx ; calculate (12+2)val1 + (51-38)val2 + (-5+20)(val4-val3), store eval2 to ebx
 
 	call DumpRegs
 	Exit
